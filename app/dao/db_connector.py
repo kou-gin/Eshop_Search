dfff37b8f72dc432f82e5735d9ab8c0fada4b1a6
kou2025 <kou.roppongi@gmail.com>
2025-06-06 14:26:54 +0900

    認証情報クリア

--- app/dao/db_connector.py
+++ app/dao/db_connector.py
@@ -1,14 +1,10 @@
-# dao/db_connector.py
 
-import mysql.connector
+import os
+from dotenv import load_dotenv
 
-def get_connection():
-    """
-    MySQL への接続を返す。接続パラメータは環境に合わせて設定してください。
-    """
-    return mysql.connector.connect(
-        host="mysql80.gins-net25.sakura.ne.jp",           # ホスト名
-        user="gins-net25_eshopapp",           # ユーザー名（例：root など）
-        password="Ba3gkC9-",   # パスワード
-        database="gins-net25_eshopapp"         # 利用するデータベース名
-    )
+load_dotenv()
+
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASS")
+database = os.getenv("DB_NAME")
